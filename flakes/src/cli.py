ffe82645a8277d720c9c572b9fc9f2c1b496d566
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-12-10 23:30:38 +0100

    fix: do not write flake.lock when running 'nix eval' from the cli

--- flakes/src/cli.py
+++ flakes/src/cli.py
@@ -10,7 +10,7 @@ import tempfile
 
 def get_cluster_config(selection = []):
     nixSelection = "".join([f"{item} = v.{item}; " for item in selection]  )
-    command = f"nix eval .#cluster --json --quiet --apply 'let pick = v: {{{nixSelection}}}; in pick'"
+    command = f"nix eval .#cluster --json --quiet --no-write-lock-file --apply 'let pick = v: {{{nixSelection}}}; in pick'"
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if result.returncode != 0:
         # Handle the error
