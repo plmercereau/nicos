9afabf1235d659629c233946a62027dca9c627b6
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-12-27 23:15:58 +0100

    chore: remove redundant option

--- hosts-nixos/pi4g.nix
+++ hosts-nixos/pi4g.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   imports = [hardware.raspberrypi-4];
-  nixpkgs.hostPlatform = "aarch64-linux";
+
   settings = {
     id = 4;
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJHOROkjkRpE/tlzhekhd4O2sMJKnBNycC/T87h+63D";
