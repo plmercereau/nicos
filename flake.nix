83acfcfeff5484825be549d40b305986db0c6deb
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-12-28 12:47:57 +0100

    docs: comments on the flake

--- flake.nix
+++ flake.nix
@@ -29,11 +29,13 @@
     cluster.lib.configure (import ./config.nix) (flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      # Use the devShells of the cluster flake
       devShells = {
         inherit (cluster.devShells.${system}) default;
       };
 
       apps = {
+        # Shortcut to call the cli using `nix run .`
         inherit (cluster.apps.${system}) default;
 
         # Browse the flake using nix repl
