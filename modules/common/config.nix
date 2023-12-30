2364c6d1abc29fbde2dcc2d9fd6277b920c4dcc3
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-12-30 22:20:20 +0100

    docs: options descriptions

--- modules/common/config.nix
+++ modules/common/config.nix
@@ -6,7 +6,11 @@
 with lib; {
   options = {
     settings.id = mkOption {
-      description = "Id of the machine, that will be translated into an IP";
+      description = ''
+        Id of the machine. Each machine must have an unique value.
+
+        This id will be translated into an IP when using the VPN module with `settings.networking.vpn.ipPrefix`.
+      '';
       type = types.int;
       readOnly = true;
     };
--- modules/keyboard/common.nix
+++ modules/keyboard/common.nix
@@ -2,7 +2,10 @@
 with lib; {
   options.settings.keyboard = {
     keyMapping = {
-      enable = mkEnableOption "special key mappings";
+      enable = mkEnableOption ''
+        special key mappings.
+              
+        On Darwin, it swaps the CapsLock key with the Control key'';
     };
   };
 }
