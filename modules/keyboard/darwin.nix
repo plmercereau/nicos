5c9150aa203b64b028a6cbcf0eac60aae6ab1b74
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2023-12-30 22:30:18 +0100

    chore: move keyboard settings to Darwin

--- modules/keyboard/darwin.nix
+++ modules/keyboard/darwin.nix
@@ -2,10 +2,24 @@
   config,
   lib,
   ...
-}: {
-  system.keyboard = lib.mkIf config.settings.keyboard.keyMapping.enable {
-    enableKeyMapping = true;
-    # Whether to remap the Caps Lock key to Control.
-    remapCapsLockToControl = true;
+}:
+with lib; {
+  options = {
+    settings.keyboard = {
+      keyMapping = {
+        enable = mkEnableOption ''
+          special key mappings.
+            
+          Swaps the CapsLock key with the Control key'';
+      };
+    };
+  };
+
+  config = {
+    system.keyboard = lib.mkIf config.settings.keyboard.keyMapping.enable {
+      enableKeyMapping = true;
+      # Whether to remap the Caps Lock key to Control.
+      remapCapsLockToControl = true;
+    };
   };
 }
