f2fe1e65d58e870722f5418a2a9809fa5d7f6407
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-01-04 21:01:33 +0100

    docs: cli links

--- cli/lib/docgen.py
+++ cli/lib/docgen.py
@@ -5,24 +5,41 @@ class MarkdownFormatter(click.HelpFormatter):
     def __init__(self, bin_cmd=None):
         super().__init__()
         self.bin_cmd = bin_cmd
+        self.is_command = False
+        self.current_usage = []
 
     def write_heading(self, heading):
         super().write(f"{'':>{self.current_indent}}### {heading}\n")
 
+    def section(self, heading):
+        self.is_command = heading == "Commands"
+        return super().section(heading)
+
     def write_usage(self, prog, args="", prefix=None):
+        # print("USAGE", prog)
+        self.current_usage = prog.split(" ")
         if self.bin_cmd:
-            split_prog = prog.split(" ")
-            split_prog[0] = self.bin_cmd
-            prog = " ".join(split_prog)
+            modified_prog = self.current_usage.copy()
+            modified_prog[0] = self.bin_cmd
+            prog = " ".join(modified_prog)
         self.write("### Usage\n\n")
         self.write("```bash\n")
         self.write(f"{prog} {args}\n")
         self.write("```\n")
 
     def write_dl(self, rows, col_max=30, col_spacing=2):
-        self.write("```\n")
-        super().write_dl(rows, col_max, col_spacing)
-        self.write("```\n")
+        if self.is_command:
+            # print("COMMAND", rows, self.current_usage)
+            anchor = "-".join(self.current_usage)
+            self.write("|     |     |\n")
+            self.write("| --- | --- |\n")
+            for [command, description] in rows:
+                self.write(f"| [{command}](#{anchor}-{command}) | {description} |\n")
+
+        else:
+            self.write("```\n")
+            super().write_dl(rows, col_max, col_spacing)
+            self.write("```\n")
 
 
 def recurse(value, path=[], bin_cmd=None):
@@ -50,4 +67,3 @@ def docgen(ctx, bin_cmd):
     main = ctx.find_root().command
 
     print(recurse(main, path=[], bin_cmd=bin_cmd))
-    # recurse(main, path=[], bin_cmd=bin_cmd)
