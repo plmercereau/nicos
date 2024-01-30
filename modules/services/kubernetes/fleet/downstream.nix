1ae8da74529b7cae5f423d3529c36895dd851e54
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-01-30 19:03:03 +0100

    feat: send kubeconfig to the upstream fleet machine

--- modules/services/kubernetes/fleet/downstream.nix
+++ modules/services/kubernetes/fleet/downstream.nix
@@ -15,32 +15,41 @@ with lib; let
     (host: host.nixpkgs.hostPlatform.isLinux && host.settings.services.kubernetes.fleet.mode == "upstream")
     (builtins.throw "No upstream machine found")
     (attrValues cluster.hosts);
-
-  script = pkgs.writeScript "send-fleet-kubeconfig" ''
-    FLAG_FILE="/var/lib/nicos/fleet.ok"
-    if [[ -e "$FLAG_FILE" ]]; then
-        echo "Script has already run. Exiting."
-        exit 0
-    fi
-    ${pkgs.openssh}/bin/ssh -i /etc/ssh/ssh_host_ed25519_key ${cfg.connectionUser}@${upstreamMachine.lib.vpn.ip} "$(cat /etc/rancher/k3s/k3s.yaml)"
-    # Create flag file to indicate script has run
-    mkdir -p "$(dirname "$FLAG_FILE")"
-    touch "$FLAG_FILE"
-  '';
 in {
-  # TODO assertion: an active upstream machine exists if the machine is downstream
   config = mkIf (k8s.enable && cfg.enable && isDownstream) {
     systemd.services.sendFleetKubeConfigInUpstream = {
       wantedBy = ["multi-user.target"];
       after = ["network.target"];
       description = "Send the kubeconfig of the machine to the upstream server.";
       serviceConfig = {
-        # TODO not correctly configured
         Type = "simple";
-        ExecStart = ''${script}'';
+        ExecStart = let
+          lockFile = "/var/lib/nicos/fleet.ok";
+        in
+          pkgs.writeShellScript "send-fleet-kubeconfig" ''
+            if [[ -e "${lockFile}" ]]; then
+                echo "Script has already run. Exiting."
+                exit 0
+            fi
+            while true; do
+            # Replace 'your_command_here' with the actual command you want to run
+            if ${pkgs.openssh}/bin/ssh -i /etc/ssh/ssh_host_ed25519_key ${cfg.connectionUser}@${upstreamMachine.lib.vpn.ip} "$(cat /etc/rancher/k3s/k3s.yaml)"; then
+              # Create flag file to indicate script has run
+              mkdir -p "$(dirname "${lockFile}")"
+              touch "${lockFile}"
+              echo "Secret pushed to the upstream server. Created ${lockFile} to indicate script has run. Exiting."
+              exit 0
+            else
+              # Capture and print the error
+              error=$?
+              echo "Command failed with error code $error. Retrying in 10 seconds..."
+              sleep 10
+            fi
+            done
+          '';
         Restart = "on-failure";
         RestartSec = 60;
-        # RemainAfterExit = "no";
+        RemainAfterExit = "no";
       };
     };
   };
