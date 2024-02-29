5cd217eba9beb4f1e3a2d5f67a6d595bb3d04a7c
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-02-29 14:14:09 +0100

    refactor: simplify

--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   imports = [
     ./fleet
-    ./fs.nix
     ./git
     ./impermanence.nix
     ./kubernetes
@@ -13,7 +12,6 @@
     ./prometheus
     ./ssh.nix
     ./swap.nix
-    ./time.nix
     ./users.nix
   ];
   system.stateVersion = "23.11";
@@ -22,4 +20,30 @@
 
   # Packages that should always be available for manual intervention
   environment.systemPackages = with pkgs; [curl e2fsprogs];
+
+  services = {
+    # https://man7.org/linux/man-pages/man8/fstrim.8.html
+    fstrim.enable = true;
+
+    # Avoid pulling in unneeded dependencies
+    udisks2.enable = lib.mkDefault false;
+
+    # NTP time sync.
+    timesyncd = {
+      enable = true;
+      servers = mkDefault [
+        "0.nixos.pool.ntp.org"
+        "1.nixos.pool.ntp.org"
+        "2.nixos.pool.ntp.org"
+        "3.nixos.pool.ntp.org"
+        "time.windows.com"
+        "time.google.com"
+      ];
+    };
+
+    htpdate = {
+      enable = true;
+      servers = ["www.kernel.org" "www.google.com" "www.cloudflare.com"];
+    };
+  };
 }
